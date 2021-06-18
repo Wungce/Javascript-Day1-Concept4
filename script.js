8d772f2b3fa119625a1e821651756d54209509e1
wangsh <wugcelama19@gmail.com>
2021-06-18 14:01:38 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const MyInput = document.createElement('input');
+document.body.appendChild(MyInput);
+
+const MyButton = document.createElement('button');
+document.body.appendChild(MyButton);
+MyButton.innerHTML="Button";
